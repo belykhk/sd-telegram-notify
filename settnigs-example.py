acbf7da01bcfbdfcf96fb3af4af0dc8c1d4937c7
Kostya Belykh <belykhk@users.noreply.github.com>
2018-07-02 13:15:38 +0400

    Create settnigs-example.py

---
+++ settnigs-example.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# Telegram bot api key
+# Generate a bot key here: https://core.telegram.org/bots#botfather
+tg_bot_key = "12345678:AAEvVsEzifLTuJdgfsgdfgdfGtrtdfgfvk"
+# Service Desk web url
+sd_url = "http://sd.domain.com"
+# Group chat uid to send Alerts
+group_chat_id = "-1234567"
+
+# Array of technicans: Name(as in Service Desk), Telegram UID, Telegram username 
+tech_arr = [
+["John Smith","111111111","@JSmith"],
+["Andre Gettsburg","1234008","@a.gets"],
+["Bill Keys","177532","@bkey"],
+]
+
+# Settings for SOCKS5 proxy
+proxy_use = False #True
+proxy_server = "socks.domain.com" #You also can use ip address
+proxy_port = "1080"
+proxy_username = "Username"
+proxy_password = "Password"
+
+# Define High priority name
+high_priority = "High"
+# Stickers set used to send messages in group chat if ticket has High priority
+# You can get sticker id by sending sticker to @getStickerId_bot in telegram
+stickers = [
+	"CAADAgADNAADaJpdDP1SVakiyHyMAg",
+	"CAADAgAD2wMAAnKq5gRdErxINUlIUwI",
+	"CAADAgADdgAD8MPADsZS8O4QuntjAg",
+	"CAADAgADUgAD4aRlBZPSu87Bys-HAg",
+	"CAADAgADfwADNmLjBffW6fOkSgklAg",
+	"CAADAgAD1wEAAoqMcQMF4GdwqWLCOAI",
+	"CAADAgADcgADZf-SCg8ipjq3H_QXAg",
+	"CAADAgADdQADZf-SCslVlwgAAToaGwI",
+	"CAADAgAD-AEAAtY1EgAB4JnqKPOMbE8C",
+	"CAADAgAD-QEAAtY1EgABBGioFXgT8-kC",
+	"CAADAgAD7wADcqrmBE6HbRTJbkh-Ag",
+	"CAADAgADfQADZf-SCl_ZY5TwLB26Ag",
+	"CAADAgADhwADZf-SCtG2Yi5ooHOpAg",
+	"CAADAgADjQEAAj-xbAO8tkFYzW54AQI",
+	"CAADBAAD-QAD0N4FAAG0aduQJRbaqQI",
+	"CAADAgADXgEAAnELQgVcPHN5QLpHQgI",
+	"CAADAgADzAAD0QXIB6p8rdBJgsjtAg",
+	"CAADAgAD1AAD0QXIB59CmV7E9qknAg",
+	"CAADAgAD9gAD0QXIBx3upigrkW0YAg",
+	"CAADAgADAQEAAtEFyAe9TCvRgnNFnQI",
+	"CAADAgADVwUAApkvSwqnSTPeEGQcxwI",
+	"CAADAgADSwUAApkvSwpf-ZLs0vl4FQI",
+	"CAADAgADPgADaJpdDBmx-BngWdVLAg",
+	"CAADAgADlwADaJpdDCx3FYlSLk88Ag",
+	"CAADAgADFgADfNZdB29MuVWm5AffAg",
+	]
